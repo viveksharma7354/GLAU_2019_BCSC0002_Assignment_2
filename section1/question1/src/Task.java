4d1ea999d41f067c1d07abddc6c1a8263e69126f
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-25 23:13:09 +0530

    added Solution of question 1 java : question1/task.java

--- section1/question1/src/Task.java
+++ section1/question1/src/Task.java
@@ -1,10 +1,6 @@
 public class Task {
 
     public int addTwoNumbers(int a, int b) {
-        // TODO write your code here, and delete this line afterwards!
-        return 0;
+        return a + b;
     }
 }
-/*
- * WARNING: DO NOT ADD A MAIN METHOD TO THIS FILE OR ANYWHERE ELSE!
- * */
