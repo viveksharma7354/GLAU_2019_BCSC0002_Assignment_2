d97836076cfcafddeb3d5d46eaa8a007ed8d8e4f
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-25 23:20:56 +0530

    added Solution of question 10 java : question10/task.java

--- section1/question10/src/Task.java
+++ section1/question10/src/Task.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
-
 public class Task {
 	public char asciiToChar(int ascii) {
 		return (char) ascii;
-	}
-
-	public void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		char b = asciiToChar(n);
-		System.out.println(b);
 	}
 }
