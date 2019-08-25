b67e7cee629ead09bd3dc6de97796f888e6fb072
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-25 20:48:36 +0530

    added Solution of question 9 java : question9/task.java

--- section1/question9/src/Task.java
+++ section1/question9/src/Task.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Task {
-    // TODO write your method here and delete this line afterwards!
+    public double maximumOfThreeNumbers(double n1, double n2, double n3) {
+        if ((n1 > n2) && (n1 > n3)) {
+            return n1;
+        } else if ((n2 > n1) && (n2 > n3)) {
+            return n2;
+        } else {
+            return n3;
+        }
+    }
+
+    public void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double n1 = s.nextDouble();
+        double n2 = s.nextDouble();
+        double n3 = s.nextDouble();
+        double b = maximumOfThreeNumbers(n1, n2, n3);
+        System.out.println(b);
+    }
 }
