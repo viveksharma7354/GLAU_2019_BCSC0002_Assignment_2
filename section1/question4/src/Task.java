b3e672e4a24a72ed447a5fa7a2d3964592326d2f
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-25 22:56:50 +0530

    added Solution of question 4 java : question4/task.java

--- section1/question4/src/Task.java
+++ section1/question4/src/Task.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Task {
-    public String convertIntegerToBinaryString(int decimalNumber) {
-        return "";
+    public static String convertIntegerToBinaryString(int decimalNumber) {
+        String c = Integer.toBinaryString(decimalNumber);
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int decimalNumber = s.nextInt();
+        String res = convertIntegerToBinaryString(decimalNumber);
+        System.out.println(res);
     }
 }
