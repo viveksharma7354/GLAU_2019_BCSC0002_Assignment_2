eca8c05af10ec7eefce4a710c9aceb42666e09f8
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-25 23:15:09 +0530

    added Solution of question 2 java : question2/task.java

--- section1/question2/src/Task.java
+++ section1/question2/src/Task.java
@@ -1,15 +1,6 @@
-import java.util.Scanner;
-
 public class Task {
     public static double calculateCircumference(double radius) {
         double circumference = 2.0 * Math.PI * radius;
         return circumference;
-    }
-
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        double radius = s.nextDouble();
-        double circumference = calculateCircumference(radius);
-        System.out.println(circumference);
     }
 }
