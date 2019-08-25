d433b79f15d8b988203b6cdf97855d0e50242c1a
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-25 23:32:27 +0530

    added Solution of question 8 java : question8/task.java

--- section1/question8/src/Task.java
+++ section1/question8/src/Task.java
@@ -1,18 +1,19 @@
 public class Task {
     public static void main(String[] args) {
         Task task = new Task();
-        StringBuilder sb = new StringBuilder();
+        String sb = "";
         String stringVal = task.booleanAnd(0, 0);
         int diff = 8 - stringVal.length();
         for (int i = 0; i < diff; i++) {
-            sb.append("0");
+            sb += "0";
         }
-        sb.append(stringVal);
+        sb = sb + stringVal;
         System.out.println(sb.toString());
     }
 
     public String booleanAnd(int number1, int number2) {
-        return "";
+        int res = (number1 & number2);
+        return Integer.toBinaryString(res);
     }
 
 }
