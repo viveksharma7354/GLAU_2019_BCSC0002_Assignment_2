a949564807260479e5f1a88b60e5d7d648cf5519
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-25 22:53:49 +0530

    added Solution of question 6 java : question6/task.java

--- section1/question6/src/Task.java
+++ section1/question6/src/Task.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
 public class Task {
-    public double calculateAreaOfTriangleWith3Sides(double side1, double side2, double side3) {
-        return 0.0;
+    public static double calculateAreaOfTriangleWith3Sides(double side1, double side2, double side3) {
+        double sum = side1 + side2 + side3;
+        sum = sum / 2;
+        double res;
+        res = pow((sum * (sum - side3) * (sum - side1) * (sum - side2)), 0.5);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double side1 = s.nextDouble();
+        double side2 = s.nextDouble();
+        double side3 = s.nextDouble();
+        double res = calculateAreaOfTriangleWith3Sides(side1, side2, side3);
+        System.out.println(res);
     }
 }
--- section1/question7/src/Task.java
+++ section1/question7/src/Task.java
@@ -1,3 +1,11 @@
 public class Task {
-    // TODO write the method here and delete this line afterwards!
+    public static void main(String[] args) {
+        int res = (int) evaluateExpression();
+        System.out.println(res);
+    }
+
+    public static double evaluateExpression() {
+        int x = (4 * 10) + 5 * 3 * (4 + 2) + 10;
+        return (double) x;
+    }
 }
