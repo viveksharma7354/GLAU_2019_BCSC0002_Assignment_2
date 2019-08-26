853318a8ff07c9f10a5a6da18ca70480227c7a55
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-26 15:51:55 +0530

    added Solution of question 8 java : question8/task.java

--- section1/question8/src/Task.java
+++ section1/question8/src/Task.java
@@ -1,13 +1,13 @@
 public class Task {
-    public String booleanAnd(int number1, int number2) {
-        String sb = "";
-        int res = (number1 & number2);
-        String stringVal = Integer.toBinaryString(res);
-        int diff = 8 - stringVal.length();
-        for (int i = 0; i < diff; i++) {
-            sb += "0";
-        }
-        sb = sb + stringVal;
-        return sb;
-    }
+	public String booleanAnd(int number1, int number2) {
+		String sb = "";
+		int res = (number1 & number2);
+		String stringVal = Integer.toBinaryString(res);
+		int diff = 8 - stringVal.length();
+		for (int i = 0; i < diff; i++) {
+			sb += "0";
+		}
+		sb = sb + stringVal;
+		return sb;
+	}
 }
--- section1/question8/test/Tests.java
+++ section1/question8/test/Tests.java
@@ -2,21 +2,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Tests {
-    Task task = new Task();
+	Task task = new Task();
 
-    @Test
-    public void checkFor4and5() {
-        Assert.assertEquals("Input(4, 5)", "00000100", task.booleanAnd(4, 5));
-    }
+	@Test
+	public void checkFor4and5() {
+		Assert.assertEquals("Input(4, 5)", "00000100", task.booleanAnd(4, 5));
+	}
 
-    @Test
-    public void checkFor2and2() {
-        Assert.assertEquals("Input(2, 2)", "00000010", task.booleanAnd(2, 2));
-    }
+	@Test
+	public void checkFor2and2() {
+		Assert.assertEquals("Input(2, 2)", "00000010", task.booleanAnd(2, 2));
+	}
 
-    @Test
-    public void checkFor0and0() {
-        Assert.assertEquals("Input(2, 2)", "00000000", task.booleanAnd(0, 0));
-    }
+	@Test
+	public void checkFor0and0() {
+		Assert.assertEquals("Input(2, 2)", "00000000", task.booleanAnd(0, 0));
+	}
 
 }
